dc28dc95b7e9f570f3025f1414ec0a468bb0e840
Carley Tripp <carley.tripp@outlook.com>
2020-04-13 21:14:34 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,11 +1,11 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: birds
+    render json: birds, except: [:created_at, :updated_at]
   end
 
   def show
     @bird = Bird.find_by(id: params[:id])
-    render json: bird
+    render json: bird, only: [:id, :name, :species]
   end
 end
